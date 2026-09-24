23da8019a16b512515a69c1d0ea8343c0aeb23fe
thanay666 <thanay.pilot@gmail.com>
2026-09-24 13:09:08 +0530

    thanay ai logic test

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const authController = require("../controllers/authController")
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 router.get("/", (req, res) => {
 
     res.status(200).json({
